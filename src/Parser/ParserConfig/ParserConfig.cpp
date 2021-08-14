8f177f34e5338ac212591307a36d452067e8d319
MinisterAkasha <gusevworkmail@gmail.com>
2021-08-14 22:30:51 +0300

    added test hosts in ParserConfig

--- src/Parser/ParserConfig/ParserConfig.cpp
+++ src/Parser/ParserConfig/ParserConfig.cpp
@@ -5,5 +5,8 @@ ParserConfig::ParserConfig() {}
 Config* ParserConfig::parse(char* configFilePath) const {
 	Config *config = new Config();
 	std::cout << configFilePath << std::endl;
+
+	config->setNewHost(Host("127.0.0.1", 8000, "localhost"));
+	config->setNewHost(Host("400.2.3.1", 5050, "my-site.com"));
 	return config;
 }
