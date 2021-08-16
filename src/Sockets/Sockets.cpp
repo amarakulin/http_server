b7cb741020da60883d45645143863db470cfbb9c
MinisterAkasha <gusevworkmail@gmail.com>
2021-08-16 21:11:01 +0300

    add Sockets class to store pollFD's vector

---
+++ src/Sockets/Sockets.cpp
@@ -0,0 +1,43 @@
+#include "Sockets.hpp"
+
+Sockets::Sockets() {}
+
+Sockets::~Sockets() {}
+
+void	Sockets::addListenerSocket(int socket) {
+	struct pollfd tmpListener;
+
+	tmpListener.fd = socket;
+	tmpListener.events = POLLIN;
+
+	_pollFD.push_back(tmpListener);
+}
+
+void	Sockets::addClientSocket(int socket) {
+	struct pollfd tmpClient;
+
+	tmpClient.fd = socket;
+	tmpClient.events = POLLIN | POLLOUT;
+
+	_pollFD.push_back(tmpClient);
+}
+
+struct pollfd 	Sockets::getSocketByFD(int fd) {
+	pollIter it = _pollFD.begin();
+	for (; (*it).fd != fd && it != _pollFD.end(); it++) ;
+	return *it;
+}
+
+struct pollfd* 	Sockets::getAllSockets() {
+	return &(_pollFD.front());
+}
+
+size_t				Sockets::size() const {
+	return _pollFD.size();
+}
+
+struct pollfd	Sockets::operator[](int fd) {
+	pollIter it = _pollFD.begin();
+	for (; (*it).fd != fd || it != _pollFD.end(); it++) ;
+	return *it;
+}
