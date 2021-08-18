0c00da3239380b7198302b6b2eb0c97f8e95a443
MinisterAkasha <gusevworkmail@gmail.com>
2021-08-18 15:33:48 +0300

    Request: add status managment to request

--- src/Request/Request.cpp
+++ src/Request/Request.cpp
@@ -1,26 +1,23 @@
 #include "Request.hpp"
 
-Request::Request() : _status(READING), _data() {
-	// std::cout << "			REQUEST			" << std::endl;	
-}
+Request::Request() : _status(NO_REQUEST), _data() {}
 
-Request::Request(const Request& other) : _data(other._data) {
-}
+Request::Request(const Request& other) : _data(other._data) {}
 
 Request::~Request() {}
 
 void	Request::addRequestChunk(std::string chunk) {
-	// _data += chunk;
-	_data.insert(_data.length(), chunk);
-	// std::cout << "|" << _data << "|" << std::endl;
-	// isDone();
+	_data += chunk;
+	
+	if (isDone())
+		_status = DONE;
+	else
+		_status = READING;
 }
 
 bool	Request::isDone() {
 	if (_data.find("\r\n\r\n") == std::string::npos)
 		return false;
-
-	// _status = DONE;
 	return true;
 }
 
