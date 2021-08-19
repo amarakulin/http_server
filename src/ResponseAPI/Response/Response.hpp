c6e386f6ba0f4af2ec48f336965319c5e6130b52
MinisterAkasha <gusevworkmail@gmail.com>
2021-08-19 15:40:18 +0300

    Response: refact data struct

--- src/ResponseAPI/Response/Response.hpp
+++ src/ResponseAPI/Response/Response.hpp
@@ -5,10 +5,17 @@
 
 // Здесь понадобиться fork и excve???
 
+typedef struct {
+	std::string	head;
+	std::string	body;
+}	ResponseData;
+
 class Response {
 	protected:
-		std::string _head;
-		std::string _body;
+		ResponseData _data;
+
+		size_t		_sendedData;
+		size_t		_leftDataToSend;
 
 	public:
 		Response();
