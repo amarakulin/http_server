d6ae3df176857622a0b9f3d46c37677f31947a2f
MinisterAkasha <gusevworkmail@gmail.com>
2021-08-19 17:43:49 +0300

    fix: add bzero to buf

--- src/Server/Server.cpp
+++ src/Server/Server.cpp
@@ -156,6 +156,7 @@ void				Server::processingRequest(int clientSocket, Client& client) {
 	}
 
 	client.getRequest()->addRequestChunk(buf);
+	bzero(buf, MB);
 
 	std::cout << "/* Client in */ " << std::endl;
 }
@@ -166,6 +167,7 @@ void				Server::createResponse(Client& client) {
 }
 
 void				Server::sendResponse(int clientSocket, Client& client) {
+	// std::string response = "HTTP/1.1 200 OK\r\nContent-length: 436\r\nContent-type: text/html\r\nDate: Wed, 21 Oct 2015 07:28:00 GMT\r\n\r\n<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Document</title><link rel='stylesheet' href='index.css'></head><body><h2>Hello</h2><form method='POST' action='127.0.0.1'><input name='value' value='key' placeholder='TEST'><button>POST</button></form><script src='index.js'></script></body></html>";
 	std::string response = "HTTP/1.1 200 OK\r\nContent-length: 318\r\nContent-type: text/html\r\nDate: Wed, 21 Oct 2015 07:28:00 GMT\r\n\r\n<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Document</title><link rel='stylesheet' href='index.css'></head><body><h2>Hello</h2><script src='index.js'></script></body></html>";
 	// std::string response = "HTTP/1.1 200 OK\r\nContent-length: 5\r\nContent-type: text/html\r\nDate: Wed, 21 Oct 2015 07:28:00 GMT\r\n\r\n12345";
 	int s = send(clientSocket, response.c_str(), response.length(), 0);
