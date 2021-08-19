3dfa5bd07b1c84f3e28b13931506b25f0df4a069
MinisterAkasha <gusevworkmail@gmail.com>
2021-08-19 15:33:07 +0300

    ResponsePost: add basic constructors/destructor

--- src/ResponseAPI/ResponsePost/ResponsePost.cpp
+++ src/ResponseAPI/ResponsePost/ResponsePost.cpp
@@ -1,1 +1,11 @@
 #include "ResponsePost.hpp"
+
+ResponsePost::ResponsePost() : Response() {}
+
+ResponsePost::ResponsePost(const ResponsePost& other) : Response(other) {}
+
+ResponsePost::~ResponsePost() {}
+
+std::string	ResponsePost::createBody() {
+	return "";
+}
