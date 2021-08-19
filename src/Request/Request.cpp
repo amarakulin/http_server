1d85399f11c184db305d130b5cfc26a553fbcdff
MinisterAkasha <gusevworkmail@gmail.com>
2021-08-19 18:27:00 +0300

    Request: add RequestReadingData support struct

--- src/Request/Request.cpp
+++ src/Request/Request.cpp
@@ -1,17 +1,21 @@
 #include "Request.hpp"
 
-Request::Request() : _status(NO_REQUEST), _data() {}
+Request::Request()
+	: _status(NO_REQUEST), _data(), _sup() {}
 
-Request::Request(const Request& other) : _data(other._data) {}
+Request::Request(const Request& other)
+	: _status(other._status), _data(other._data), _sup() {}
 
 Request::~Request() {}
 
 void		Request::addRequestChunk(std::string chunk) {
 	_data += chunk;
 	
+	if (!_sup.method.length())
+		parseMethod();
+
 	if (isDone()) {
 		_status = READED;
-		parseMethod();
 	} else {
 		_status = READING;
 	}
@@ -19,23 +23,27 @@ void		Request::addRequestChunk(std::string chunk) {
 
 void		Request::parseMethod() {
 	if (_data.find("GET") != std::string::npos)
-		_method = "GET";
+		_sup.method = "GET";
 	else if (_data.find("POST") != std::string::npos)
-		_method = "POST";
+		_sup.method = "POST";
 	else if (_data.find("DELETE") != std::string::npos)
-		_method = "DELETE";
+		_sup.method = "DELETE";
 }
 
 bool		Request::isDone() {
-	if (_data.find("\r\n\r\n") == std::string::npos)
-		return false;
+	if (_sup.method == "GET" || _sup.method == "DELETE") {
+		if (_data.find("\r\n\r\n") == std::string::npos)
+			return false;
+	} else if (_sup.method == "POST") {
+		
+	}
 	return true;
 }
 
 void		Request::resetRequest() {
 	_status = NO_REQUEST;
 	_data = "";
-	_method = "";
+	_sup.method = "";
 }
 
 int			Request::getStatus() const {
@@ -43,5 +51,5 @@ int			Request::getStatus() const {
 }
 
 std::string	Request::getMethod() const {
-	return _method;
+	return _sup.method;
 }
--- src/Request/Request.hpp
+++ src/Request/Request.hpp
@@ -5,11 +5,19 @@
 
 typedef enum { NO_REQUEST, READING, READED } requestStatus;
 
+typedef struct {
+	std::string method;
+	std::string boundary;
+	size_t		contentLenght;
+	bool		isChunked;
+} RequestReadingData;
+
 class Request {
 	private:
-		std::string _method;
-		std::string	_data;
 		int			_status;
+		std::string	_data;
+
+		RequestReadingData _sup;
 
 	public:
 		Request();
