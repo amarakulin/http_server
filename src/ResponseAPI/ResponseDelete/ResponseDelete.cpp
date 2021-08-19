3d36959ed812bbf983ee71f31ed0cb83114947ad
MinisterAkasha <gusevworkmail@gmail.com>
2021-08-19 15:33:20 +0300

    ResponseDelete: add basic constructors/destructor

--- src/ResponseAPI/ResponseDelete/ResponseDelete.cpp
+++ src/ResponseAPI/ResponseDelete/ResponseDelete.cpp
@@ -1,1 +1,11 @@
 #include "ResponseDelete.hpp"
+
+ResponseDelete::ResponseDelete() : Response() {}
+
+ResponseDelete::ResponseDelete(const ResponseDelete& other) : Response(other) {}
+
+ResponseDelete::~ResponseDelete() {}
+
+std::string	ResponseDelete::createBody() {
+	return "";
+}
