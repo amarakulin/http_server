77219949a9ac6fef8079e69435626b92b1c0ca39
MinisterAkasha <gusevworkmail@gmail.com>
2021-08-20 17:16:43 +0300

    typedef: add requestHeaderStruct defenition

--- src/utils/typedef/typedef.hpp
+++ src/utils/typedef/typedef.hpp
@@ -5,4 +5,12 @@
 
 typedef std::map<std::string, std::string> requestHeaderStruct;
 
+/*
+** Все поля представленны в нижнем регистре
+** Обязательно будут заполнены следующие поля:
+** - method
+** - location
+** - protocol
+*/
+
 #endif
