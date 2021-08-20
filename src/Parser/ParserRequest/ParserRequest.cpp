b8c018069ede854583c3fe98bf1651110e02e702
MinisterAkasha <gusevworkmail@gmail.com>
2021-08-20 16:01:25 +0300

    ParserRequest: add basic class struct

--- src/Parser/ParserRequest/ParserRequest.cpp
+++ src/Parser/ParserRequest/ParserRequest.cpp
@@ -1,1 +1,13 @@
 #include "ParserRequest.hpp"
+
+ParserRequest::ParserRequest() {}
+
+ParserRequest::ParserRequest(const ParserRequest& other) {}
+
+ParserRequest::~ParserRequest() {}
+
+requestHeaderStruct ParserRequest::parseHeader(std::string data) {
+	requestHeaderStruct header;
+
+	return header;
+}
--- src/Parser/ParserRequest/ParserRequest.hpp
+++ src/Parser/ParserRequest/ParserRequest.hpp
@@ -1,11 +1,20 @@
 #ifndef PARSER_REQUEST
 # define PARSER_REQUEST
 
+# include "typedef.hpp"
+
+# include <iostream>
+# include <map>
+
 class Request;
 
 class ParserRequest {
 	public:
 		ParserRequest();
+		ParserRequest(const ParserRequest& other);
+		~ParserRequest();
+
+		requestHeaderStruct	parseHeader(std::string data);
 		Request& parse();
 };
 
