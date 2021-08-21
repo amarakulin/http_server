c505ff921cef65206007507cbfb52e2ae349bb6e
MinisterAkasha <gusevworkmail@gmail.com>
2021-08-22 02:19:51 +0300

    utils: add printRequest support function

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -3,3 +3,13 @@
 void	toLowerCase(std::string& str) {
 	std::transform(str.begin(), str.end(), str.begin(), ::tolower);
 }
+
+void	printRequest(RequestData request) {
+	std::cout << "<-- Header ->" << std::endl;
+	for (requestHeaderStruct::iterator it = request.header.begin(); it != request.header.end(); it++) {
+		std::cout << (*it).first << ": " << (*it).second << std::endl;
+	}
+
+	std::cout << "<-- Body ->" << std::endl;
+	std::cout << request.body << std::endl;
+}
