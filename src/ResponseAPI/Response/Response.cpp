d1ffec0aefb2f2849c2b5cbc9116261fff4b8aaf
pichkasik <sasha.marakulin@mail.ru>
2021-08-24 22:31:17 +0300

    Response: delete useless if stataments

--- src/ResponseAPI/Response/Response.cpp
+++ src/ResponseAPI/Response/Response.cpp
@@ -34,24 +34,10 @@ bool Response::isDone(){
 void Response::countSendedData(int byteSended){
 	if (byteSended < 0 || byteSended > _leftBytesToSend){
 		std::cout << "DATA SENT is negative!!!" << std::endl;
-		_status = SENDED;
-		return;
 	}
 	_leftBytesToSend -= byteSended;
 	if (_leftBytesToSend == 0 && !_dataToSend.empty()){
 		_status = SENDED;
-	}
-	else if (_leftBytesToSend == 0 && _dataToSend.empty()){//TODO delete
-		std::cout << "EMPTY SEND!!!" << std::endl;
-		return;
-	}
-	else if (_leftBytesToSend < 0){//TODO delete
-		std::cout << "1)LEFT DATA SENT error!!!" << std::endl;
-		return;
-	}
-	else if (_leftBytesToSend != 0 && _dataToSend.empty()){//TODO delete
-		std::cout << "2)LEFT DATA SENT error!!!" << std::endl;
-		return;
 	}
 	_dataToSend.erase(_dataToSend.begin(), _dataToSend.begin() + byteSended);
 }
