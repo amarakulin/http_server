64501f4800c36dcceb421e9d6f0dcedaa8b74f8f
pichkasik <sasha.marakulin@mail.ru>
2021-08-24 20:44:17 +0300

    ResponseGet: Add arguments for createHead and createBody

--- src/ResponseAPI/ResponseGet/ResponseGet.cpp
+++ src/ResponseAPI/ResponseGet/ResponseGet.cpp
@@ -4,10 +4,11 @@ ResponseGet::ResponseGet() : Response() {}
 
 ResponseGet::ResponseGet(const ResponseGet& other) : Response(other) {}
 
-ResponseGet::ResponseGet(const Request* request) : Response(request) {
+ResponseGet::ResponseGet(Request *request) : Response(request) {
 	std::cout << "Constructor ResponseGet" << std::endl;
-	std::string head = createHead();
-	std::string body = createBody();
+
+	std::string head = createHead(request->getData().header);
+	std::string body = createBody(request->getData().body);
 	_dataToSend = head + body;
 //	TODO delete hardcode
 //	_dataToSend = "HTTP/1.1 200 OK\r\nContent-length: 436\r\nContent-type: text/html\r\nDate: Wed, 21 Oct 2015 07:28:00 GMT\r\n\r\n<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Document</title><link rel='stylesheet' href='index.css'></head><body><h2>Hello</h2><form method='POST' action='127.0.0.1'><input name='value' value='key' placeholder='TEST'><button>POST</button></form><script src='index.js'></script></body></html>";
@@ -19,10 +20,10 @@ ResponseGet::ResponseGet(const Request* request) : Response(request) {
 
 ResponseGet::~ResponseGet() {}
 
-std::string ResponseGet::createBody() {
+std::string ResponseGet::createHead(const requestHeaderStruct& headers){
 	return "";
 }
 
-std::string ResponseGet::createHead(){
+std::string ResponseGet::createBody(const std::string& body) {
 	return "";
 }
--- src/ResponseAPI/ResponseGet/ResponseGet.hpp
+++ src/ResponseAPI/ResponseGet/ResponseGet.hpp
@@ -2,17 +2,18 @@
 # define RESPONSE_GET
 
 # include "../Response/Response.hpp"
+#include "../Types/RequestTypes.hpp"
 
 class ResponseGet : public Response {
 public:
 	ResponseGet();
 	ResponseGet(const ResponseGet& other);
-	ResponseGet(const Request* other);
+	ResponseGet(Request *other);
 	~ResponseGet();
 
 private:
-	std::string		createBody();
-	std::string		createHead();
+	std::string		createHead(const requestHeaderStruct& headers);
+	std::string		createBody(const std::string& body);
 };
 
 #endif
