f147024e1c892f36e0e2e6d1e241b6e0e9c0bf92
MinisterAkasha <gusevworkmail@gmail.com>
2021-08-28 21:51:38 +0300

    utils: add split and hasLowerCaseLetter utils functions

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -13,3 +13,31 @@ void	printRequest(RequestData request) {
 	std::cout << "<-- Body ->" << std::endl;
 	std::cout << request.body << std::endl;
 }
+
+std::vector<std::string>	split(std::string value, std::string delim) {
+	std::vector<std::string> tmp;
+	size_t start = 0;
+	size_t end = 0;
+
+	while (start != value.length() && end != std::string::npos) {
+		end = value.find(delim, start);
+
+		if (end == std::string::npos) {
+				tmp.push_back(value.substr(start, end - start));
+		} else {
+			if (start - end != 0)
+				tmp.push_back(value.substr(start, end - start));
+			start = end + delim.length();
+		}
+	}
+	return tmp;
+}
+
+bool	hasLowerCaseLetter(std::string& str) {
+	size_t len = str.length();
+	for (size_t i = 0; i < len; i++) {
+		if (islower(str[i]))
+			return true;
+	}
+	return false;
+}
--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -7,5 +7,7 @@
 
 void	toLowerCase(std::string& str);
 void	printRequest(RequestData request);
+std::vector<std::string>	split(std::string value, std::string delim);
+bool	hasLowerCaseLetter(std::string& str);
 
 #endif
