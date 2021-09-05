a3181a8a5459d34351b5a2bc211ed7ef15790b35
lelsweyr17 <arbur97@gmail.com>
2021-09-05 20:13:02 +0300

    Main: add try/catch to main for catching parser errors

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 #include "Server.hpp"
 
 int main(int argc, char **argv) {
-	Server server(ParserConfig().parse(argv[1]));
-	server.start(); 
+	try {
+		Server server(ParserConfig().parse(argv[1]));
+		server.start(); 
+	} catch(const std::exception& e) {
+	}
 	return 0;
 }
