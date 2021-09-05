58b41688c4a61670ee09fb30785bdbd30fa62b4c
lelsweyr17 <arbur97@gmail.com>
2021-09-05 19:56:37 +0300

    Main: delete old version of parser start, add int argc in main declaration

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include "ParserConfig.hpp"
 #include "Server.hpp"
 
-int main(int , char** argv) {
+int main(int argc, char **argv) {
 	Server server(ParserConfig().parse(argv[1]));
-	// ParserConfig().parse(argv[1]);
 	server.start(); 
 	return 0;
 }
