8576be0f7440d9ccd0f999faac402f4e05d08ce0
pichkasik <sasha.marakulin@mail.ru>
2021-09-16 17:06:22 +0300

    Utils: add function find with case insensetive

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -6,6 +6,7 @@
 # include <sstream>
 # include <sys/stat.h>
 #include "RequestTypes.hpp"
+#include "algorithm"
 
 #define RESET   "\033[0m"
 #define GREEN   "\033[32m"                 /* Green */
@@ -25,5 +26,27 @@ bool	isSomeSymbolInTheEnd(std::string end, char symbol);
 bool	isFileExist (const std::string& filePath);
 bool	isFileInPath (const std::string& filePath);
 bool	isItemInVector(std::vector<std::string> vector, const std::string& val);
+
+
+// templated version of my_equal so it could work with both char and wchar_t
+template<typename charT>
+struct my_equal {
+	my_equal( const std::locale& loc ) : loc_(loc) {}
+	bool operator()(charT ch1, charT ch2) {
+		return std::toupper(ch1, loc_) == std::toupper(ch2, loc_);
+	}
+private:
+	const std::locale& loc_;
+};
+
+// find substring (case insensitive)
+template<typename T>
+int ci_find_substr( const T& str1, const T& str2, const std::locale& loc = std::locale() )
+{
+	typename T::const_iterator it = std::search( str1.begin(), str1.end(),
+												 str2.begin(), str2.end(), my_equal<typename T::value_type>(loc) );
+	if ( it != str1.end() ) return it - str1.begin();
+	else return -1; // not found
+}
 
 #endif
